a021fff5ff33ca7d27566b5c503d333286ae8aa2
Brady Berg <bergbrae@msu.edu>
2024-06-07 17:13:38 -0400

    Add attribution to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,7 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Modal, Button } from "react-bootstrap";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
@@ -11,6 +10,12 @@ import { IoIosInformationCircle } from "react-icons/io";
 function Footer() {
   let date = new Date();
   let year = date.getFullYear();
+  
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <Container fluid className="footer">
       <Row>
@@ -51,17 +56,30 @@ function Footer() {
             </li>
           </ul>
         </Col>
-        <Col md={{ span: 1, offset: 3 }}  className="footer-info">
-          <a
-            href="https://twitter.com/bradyberg_"
-            style={{ color: "white" }}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <IoIosInformationCircle />
-          </a>
+        <Col md={{ span: 1, offset: 3 }} className="footer-info">
+          <IoIosInformationCircle style={{ color: "white" }} onClick={handleShow} />
         </Col>
       </Row>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Additional Information</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            This website was forked from a project by <a href="https://github.com/soumyajit4419" target="_blank" rel="noopener noreferrer">soumyajit4419</a>.
+            You can find the original repository <a href="https://github.com/soumyajit4419/Portfolio" target="_blank" rel="noopener noreferrer">here</a>.
+          </p>
+          <p>
+            You can find my forked repository <a href="https://github.com/bergbrae/Portfolio" target="_blank" rel="noopener noreferrer">here</a>.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 }
