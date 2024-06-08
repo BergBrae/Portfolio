8509abb16e168cc63a6e227b198ce80b35226ab0
Brady Berg <bergbrae@msu.edu>
2024-06-07 21:52:52 -0400

    Fix layout style of job cards

--- src/components/Jobs/JobCard.js
+++ src/components/Jobs/JobCard.js
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 import Accordion from 'react-bootstrap/Accordion';
+import { Row, Col, Container } from "react-bootstrap";
 
 function JobCard(props) {
   const { job, index } = props;
@@ -11,20 +12,28 @@ function JobCard(props) {
   const imagesPath = process.env.PUBLIC_URL + "/assets/images/";
 
   return (
-    <Accordion.Item eventKey={index} className="transparent">
+    <Accordion.Item eventKey={index} className="transparent job-item">
       <Accordion.Header >
-        <img src={imagesPath + Image} className="img-fluid white-on-transparent" style={{ maxHeight: "80px" }} />
-        <h2>
-          {Title}
-        </h2>
-        <h3>
-          {Company}
-        </h3>
+        <Container>
+          <Row>
+            <Col md={2}>
+              <img src={imagesPath + Image} className="img-fluid white-on-transparent m-4" style={{ maxHeight: "100px", maxWidth: "100px" }} />
+            </Col>
+            <Col md={6}>
+              <h2>
+                {Title}
+              </h2>
+              <p>
+                {Company}
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </Accordion.Header>
+      <Accordion.Body>
         <p>
           {Duration}
         </p>
-      </Accordion.Header>
-      <Accordion.Body>
         <ul>
           {Responsibilities.map((responsibility, index) => {
             return <li key={index}>{responsibility}</li>;
